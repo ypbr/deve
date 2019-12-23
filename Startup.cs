20fbba970a8c23e69821591556da343782d31d63
ypbr <ypbr@outlook.com>
2019-12-23 22:23:08 +0300

    MVC view and controller files has been added

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,8 @@ namespace deve
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +32,7 @@ namespace deve
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+               endpoints.MapControllerRoute(name: "Default", pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
